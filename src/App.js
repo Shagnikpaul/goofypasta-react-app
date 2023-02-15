3737e7dad12c9ba3c464db3e704ffd1076c1c023
Shagnik <shagnikpaul.772@gmail.com>
2023-02-15 23:05:12 +0530

    css stuff changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <Navbar topTitle="Keke" kok="JOE"/>
-      <About />
+      <div>
+        <Navbar topTitle="Keke" kok="JOE" />
+        <About />
+      </div>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function About(props) {
   return (
-    <div>
+    <div className="row align-items-end bcgl">
       <h1 className="display-1 text-center">About me.</h1>
-      <figure className="text-center">
+      <figure className="text-center mt-n5">
         <blockquote className="blockquote">
           <p>Ming Dynasty Pussy, Got me acting unwise.</p>
         </blockquote>
@@ -13,6 +13,11 @@ export default function About(props) {
           Emperor Xuanzang, cited from <cite title="Source Title">Bible</cite>
         </figcaption>
       </figure>
+      <div className="d-grid gap-2 col-2 mx-auto mt-5 col">
+        <button className="btn btn-primary p-3" type="button">
+          Click here to display a random quote.
+        </button>
+      </div>
     </div>
   );
 }
