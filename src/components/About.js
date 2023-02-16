4d75646c36e438f46bdb2d68e590fba30dcaae79
Shagnik <shagnikpaul.772@gmail.com>
2023-02-16 23:05:34 +0530

    functionality added

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,60 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 
 export default function About(props) {
+  const emojis = [
+    "😀",
+    "😂",
+    "🤣",
+    "😍",
+    "😎",
+    "🤔",
+    "😴",
+    "🤯",
+    "👀",
+    "🙌",
+    "🤩",
+    "🥳",
+    "🤪",
+    "😇",
+    "😋",
+    "🥰",
+    "😷",
+    "🤗",
+    "😢",
+    "🤫"
+  ];
+
+  const incCount = () => {
+    setCountText(countText + 1);
+    const k = emojis[Math.floor(Math.random() * emojis.length)];
+    setEmoji(k);
+  };
+
+  const [countText, setCountText] = useState(0);
+  const [emoji, setEmoji] = useState("💖");
   return (
     <div className="d-flex justify-content-around flex-column mb-3 vh-100">
       <div className="p-2">
-        <h1 className="display-1 text-center">About me.</h1>
+        <h1 className="display-1 text-center"> {emoji} About me.</h1>
       </div>
-      <div className="p-2 flex-grow">
+      <div className="p-1">
         <figure className="text-center mt-n5">
           <blockquote className="blockquote">
             <p>Ming Dynasty Pussy, Got me acting unwise.</p>
           </blockquote>
           <figcaption className="blockquote-footer">
-            Emperor Xuanzang, cited from <cite title="Source Title">Bible</cite>
+            Emperor Xuanzang, cited from{" "}
+            <cite title="Source Titles">Bible</cite>
           </figcaption>
         </figure>
       </div>
       <div className="p-2 flex-row d-flex justify-content-center">
-        <button type="button" className="btn btn-primary btn-lg p-3">
-          Click here to get a random quote.
+        <button
+          type="button"
+          className="btn btn-primary btn-lg p-3"
+          onClick={incCount}
+        >
+          Click here to get a random quote. (Used {countText} times.)
         </button>
       </div>
     </div>
