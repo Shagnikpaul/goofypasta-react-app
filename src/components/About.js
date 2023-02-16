e4a49d813098221003de137739d262d16cc9c3dc
Shagnik <shagnikpaul.772@gmail.com>
2023-02-16 11:48:03 +0530

    layout changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,23 @@ import PropTypes from "prop-types";
 
 export default function About(props) {
   return (
-    <div className="row align-items-end bcgl">
-      <h1 className="display-1 text-center">About me.</h1>
-      <figure className="text-center mt-n5">
-        <blockquote className="blockquote">
-          <p>Ming Dynasty Pussy, Got me acting unwise.</p>
-        </blockquote>
-        <figcaption className="blockquote-footer">
-          Emperor Xuanzang, cited from <cite title="Source Title">Bible</cite>
-        </figcaption>
-      </figure>
-      <div className="d-grid gap-2 col-2 mx-auto mt-5 col">
-        <button className="btn btn-primary p-3" type="button">
-          Click here to display a random quote.
+    <div className="d-flex justify-content-around flex-column mb-3 vh-100">
+      <div className="p-2">
+        <h1 className="display-1 text-center">About me.</h1>
+      </div>
+      <div className="p-2 flex-grow">
+        <figure className="text-center mt-n5">
+          <blockquote className="blockquote">
+            <p>Ming Dynasty Pussy, Got me acting unwise.</p>
+          </blockquote>
+          <figcaption className="blockquote-footer">
+            Emperor Xuanzang, cited from <cite title="Source Title">Bible</cite>
+          </figcaption>
+        </figure>
+      </div>
+      <div className="p-2 flex-row d-flex justify-content-center">
+        <button type="button" className="btn btn-primary btn-lg p-3">
+          Click here to get a random quote.
         </button>
       </div>
     </div>
